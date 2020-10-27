7f02ea13ed6eeb779d96ca2f1371aa38afe104d3
Farhan Farooq <farhan.farooq@live.com>
2020-10-27 22:43:26 +0500

    moved bootstrap imports to gatsby-browser

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "jquery/dist/jquery.min.js";
-import "popper.js/dist/popper.min";
-import "bootstrap/dist/js/bootstrap.min.js";
 
 import Layout from "../components/layout";
 import Header from "../components/header";
